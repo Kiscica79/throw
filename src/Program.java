47b513dbda3fad892741348d75fe64c0b30b0b9b
Kiscica79 <oroszildiko79@gmail.hu>
2023-06-29 13:32:50 +0200

    ready

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,13 @@
 public class Program {
 
     public static void main (String args[]) throws NegativSzamException {
-        System.out.println(Szamit.duplaz(-3.8));
+        Szamit szamitas = new Szamit();
+        try {
+            System.out.println(Szamit.duplaz(-3.8));
+        } catch (NegativSzamException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 
 
